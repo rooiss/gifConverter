c9bae5bc0bffa701ee5b4e9cd4fefe2c715ce1bb
rooiss <louis.kim010@gmail.com>
2024-03-13 22:54:11 -0700

    init project

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const path = require('path')
+const ffmpeg = require('fluent-ffmpeg')
+const multer = require('multer')
+
+const app = express()
+const upload = multer({ dest: 'uploads/' })
+
+const port = 3000
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'index.html'))
+})
+
+app.post('/uploadVideo', upload.single('video'), function (req, res) {
+  if (!req.file) {
+    return res.status(400).send('No file uploaded.')
+  }
+
+  const inputFilePath = req.file.path
+  const outputFilePath = path.join(__dirname, 'output.gif')
+
+  ffmpeg(inputFilePath)
+    .output(outputFilePath)
+    .outputOptions('-vf', 'fps=10,scale=320:-1:flags=lanczos')
+    .on('end', () => {
+      console.log('Conversion complete')
+      res.download(outputFilePath) // Download the converted GIF file
+    })
+    .on('error', (err) => {
+      console.error('Error during conversion:', err)
+      res.status(500).send('Error during conversion.')
+    })
+    .run()
+})
+
+app.listen(port, function () {
+  console.log(`listening on port ${port}!`)
+})
