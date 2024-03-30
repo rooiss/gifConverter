608eb84e359639503f684fafaadda171369235f0
rooiss <louis.kim010@gmail.com>
2024-03-29 20:06:58 -0700

    updated file size restriction from default nginx settings

--- public/upload.js
+++ public/upload.js
@@ -133,21 +133,19 @@
   })
 
   window.onclick = (event) => {
-    const modal = document.getElementById('modalContainer')
+    const modal = document.getElementById('modal')
     if (event.target == modal) {
       modal.style.display = 'none'
     }
   }
-
   const triggerModal = (e) => {
     e.preventDefault()
-    const modal = document.getElementById('modalContainer')
+    const modal = document.getElementById('modal')
     modal.style.display = 'block'
 
     const submitInput = document.getElementById('submitInput')
     submitInput.click()
   }
-  // validations
   const isValidVidWidth = (width) => {
     return width !== '' && width >= 10 && width <= 320
   }
