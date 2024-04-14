ba8962bcd79d2a71a417644b7c53a2cc6473bd32
rooiss <louis.kim010@gmail.com>
2024-04-14 12:03:02 -0700

    added complete icon to steps, fixed thumbnail ratio

--- public/upload.js
+++ public/upload.js
@@ -1,10 +1,24 @@
 ;(function () {
   // global variables
-  // hold the file
+  // holds the file
   let droppedFile
   let currentStep = 1
   const OUTPUT_TYPES = new Set()
   OUTPUT_TYPES.add('gif')
+  const COMPLETED_ICON = `
+      <svg
+        fill="#000000"
+        width="18px"
+        height=18px"
+        viewBox="0 0 96 96"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <title />
+        <g>
+          <path d="M58.3945,32.1563,42.9961,50.625l-5.3906-6.4629a5.995,5.995,0,1,0-9.211,7.6758l9.9961,12a5.9914,5.9914,0,0,0,9.211.0059l20.0039-24a5.9988,5.9988,0,1,0-9.211-7.6875Z" fill="#5cb85c"/>
+          <path d="M48,0A48,48,0,1,0,96,48,48.0512,48.0512,0,0,0,48,0Zm0,84A36,36,0,1,1,84,48,36.0393,36.0393,0,0,1,48,84Z" fill="#5cb85c"/>
+        </g>
+      </svg>`
 
   const STEPS = {
     '1': {
@@ -50,7 +64,7 @@
     '2': {
       init: (template) => {
         // add event listeners to elements in the template
-        const data = {}
+        const data = { completeIcon: COMPLETED_ICON }
         const newMarkup = interpolate(template, data)
         document.getElementById('action-area').innerHTML = newMarkup
 
@@ -71,6 +85,7 @@
     },
     '3': {
       init: (template, data) => {
+        data.completeIcon = COMPLETED_ICON
         const newMarkup = interpolate(template, data)
         document.getElementById('action-area').innerHTML = newMarkup
         handleUpload(data)
@@ -79,6 +94,7 @@
     },
     '4': {
       init: (template, data) => {
+        data.completeIcon = COMPLETED_ICON
         const newMarkup = interpolate(template, data)
         document.getElementById('action-area').innerHTML = newMarkup
         document
@@ -154,12 +170,14 @@
         let canvasWidth = video.videoWidth
         let canvasHeight = video.videoHeight
 
+        // greater than 1 is a horizontal video
+        // less than 1 is a vertical video
         if (aspectRatio > 1) {
           canvasWidth = thumbnail.clientWidth
           canvasHeight = thumbnail.clientWidth / aspectRatio
         } else {
-          canvasWidth = thumbnail.clientHeight * aspectRatio
-          canvasHeight = thumbnail.clientHeight
+          canvasWidth = 100
+          canvasHeight = canvasWidth / aspectRatio
         }
 
         canvas.width = canvasWidth
